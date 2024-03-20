45a61c3b3ec84d91d23b8cffc58de14279810ad5
Emil <50961259+EmK530@users.noreply.github.com>
2024-03-20 20:38:10 +0100

    kill unused code

--- Luau/Essentials.cs
+++ Luau/Essentials.cs
@@ -34,25 +34,6 @@ public struct Proto
 
 public static class ParseEssentials
 {
-    public static Proto luaF_newproto()
-    {
-        Proto f = new Proto();
-        f.maxstacksize = 0;
-        f.numparams = 0;
-        f.nups = 0;
-        f.is_vararg = 0;
-        f.k = null;
-        f.sizek = 0;
-        f.p = null;
-        f.sizep = 0;
-        f.code = null;
-        f.sizecode = 0;
-        f.flags = 0;
-        f.codeentry = null;
-        f.registers = new object[255];
-        f.instpos = -1;
-        return f;
-    }
     public static Proto PrepareProto(ByteReader br, int i, string[] tbl)
     {
         Proto p = new Proto();
